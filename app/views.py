9ae2359f1be0043435a0efc30c35fb6af15cac66
matt vanderhoff <m.vanderhoff94@gmail.com>
2017-03-16 21:15:20 -0400

    added individual switch control
    also started the work on registration and error pages

--- app/views.py
+++ app/views.py
@@ -14,18 +14,44 @@ def index():
 @app.route('/off')
 def switch_off():
     network_id = request.args.get('netid')
+    switch = request.args.get('switch')
     print("toggling {}".format(network_id))
-    sub.call('hacklet off -n {} -s 0'.format(network_id), shell=True)
-    sub.call('hacklet off -n {} -s 1'.format(network_id), shell=True)
+    if network_id is None:
+        return error("no netid provided")
+    if switch is None:
+        sub.call('hacklet off -n {} -s 0'.format(network_id), shell=True)
+        sub.call('hacklet off -n {} -s 1'.format(network_id), shell=True)
+    if switch == "1" or switch == "0" :
+        sub.call('hacklet off -n {} -s {}'.format(network_id,switch), shell=True)
+    else:
+        return error("invalid switch provided")
     return index()
 
 @app.route('/on')
 def switch_on():
     network_id = request.args.get('netid')
+    switch = request.args.get('switch')
     print("toggling {}".format(network_id))
-    sub.call('hacklet on -n {} -s 0'.format(network_id), shell=True)
-    sub.call('hacklet on -n {} -s 1'.format(network_id), shell=True)
+    if network_id is None:
+        return error("no netid provided")
+    if switch is None:
+        sub.call('hacklet on -n {} -s 0'.format(network_id), shell=True)
+        sub.call('hacklet on -n {} -s 1'.format(network_id), shell=True)
+    if switch == "1" or switch == "0" :
+        sub.call('hacklet on -n {} -s {}'.format(network_id,switch), shell=True)
+    else:
+        return error("invalid swithch provided")
     return index()
+
+@app.route('/error')
+def error():
+    title = "error"
+    return render_template("error.html", title=title)
+
+@app.route('/register')
+def register():
+    title = "register"
+    return render_template("register.html", title=title)
 
 def read_switches():
     switch_file = open("data/switches.json")
